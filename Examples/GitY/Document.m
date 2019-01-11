a5bbd134398db558cc705799fb5fb4b8c9b70fb8
chikami <chikami@exdit.com>
2019-01-11 16:15:18 +0900

    single commit tab, command line args

--- Examples/GitY/Document.m
+++ Examples/GitY/Document.m
@@ -44,7 +44,8 @@
 
 - (BOOL)readFromURL:(NSURL*)url ofType:(NSString*)typeName error:(NSError**)outError {
   BOOL success = NO;
-  _repository = [[GCLiveRepository alloc] initWithExistingLocalRepository:url.path error:outError];
+    
+    _repository = [[GCLiveRepository alloc] initWithExistingLocalRepository:url.path error:outError];
   if (_repository) {
     if (_repository.bare) {
       if (outError) {
@@ -70,27 +71,28 @@
   [self addWindowController:_windowController];
 }
 
+
 - (void)windowControllerDidLoadNib:(NSWindowController*)aController {
   [super windowControllerDidLoadNib:aController];
 
-  _toolbar = [[NSToolbar alloc] initWithIdentifier:@"default"];
-  _toolbar.delegate = self;
-  _windowController.window.toolbar = _toolbar;
-
-  NSSortDescriptor* descriptor = [[NSSortDescriptor alloc] initWithKey:@"date" ascending:NO selector:@selector(compare:)];
-  _arrayController.sortDescriptors = @[ descriptor ];
-
-  _diffContentsViewController = [[GIDiffContentsViewController alloc] initWithRepository:_repository];
-  _diffContentsViewController.delegate = self;
-  _diffContentsViewController.headerView = _headerView;
-  _diffContentsViewController.view.frame = _diffView.frame;
-  [_diffView.superview replaceSubview:_diffView with:_diffContentsViewController.view];
-
+    NSArray *argv = [[NSProcessInfo processInfo] arguments];
+    NSArray *args = [argv subarrayWithRange:NSMakeRange(1, argv.count - 1)];
+    NSLog(@"git:args:%@", args);
+    NSString *message = @"";
+    for (NSString *arg in args) {
+        if ([arg hasPrefix:@"--message="]) {
+            message = [arg substringWithRange:NSMakeRange(10, arg.length-10)];
+        }
+    }
+    
+   NSLog(@"git:message:%@", message);
+    
   _commitViewController = [[GIAdvancedCommitViewController alloc] initWithRepository:_repository];
-  [[_tabView tabViewItemAtIndex:1] setView:_commitViewController.view];
+    [_commitViewController stageAllFiles];
+    _commitViewController.messageTextView.string = message;
+    [_commitViewController.messageTextView moveToEndOfLine:nil];
+  [[_tabView tabViewItemAtIndex:0] setView:_commitViewController.view];
 
-  _headerViewMinHeight = _headerView.frame.size.height - _messageTextField.frame.size.height;
-  _messageTextFieldMargins = _headerView.frame.size.width - _messageTextField.frame.size.width;
 
   [self repositoryDidUpdateHistory:nil];
 }
@@ -98,47 +100,6 @@
 // Override -updateChangeCount: which is trigged by NSUndoManager to do nothing and not mark document as updated
 - (void)updateChangeCount:(NSDocumentChangeType)change {
   ;
-}
-
-#pragma mark - NSToolbarDelegate
-
-- (NSToolbarItem*)toolbar:(NSToolbar*)toolbar itemForItemIdentifier:(NSString*)identifier willBeInsertedIntoToolbar:(BOOL)flag {
-  NSToolbarItem* item = [[NSToolbarItem alloc] initWithItemIdentifier:identifier];
-  if ([identifier isEqualToString:kToolbarItem_LeftView]) {
-    item.view = _leftToolbarView;
-    item.label = NSLocalizedString(@"View", nil);
-  } else if ([identifier isEqualToString:kToolbarItem_RightView]) {
-    item.view = _rightToolbarView;
-    item.label = NSLocalizedString(@"Search", nil);
-  }
-  return item;
-}
-
-- (NSArray*)toolbarDefaultItemIdentifiers:(NSToolbar*)toolbar {
-  return @[ kToolbarItem_LeftView, NSToolbarFlexibleSpaceItemIdentifier, kToolbarItem_RightView ];
-}
-
-- (NSArray*)toolbarAllowedItemIdentifiers:(NSToolbar*)toolbar {
-  return [self toolbarDefaultItemIdentifiers:toolbar];
-}
-
-#pragma mark - NSTableViewDelegate
-
-- (void)tableViewSelectionDidChange:(NSNotification*)notification {
-  GCHistoryCommit* commit = _arrayController.selectedObjects.firstObject;
-  if (commit) {
-    _currentDiff = [_repository diffCommit:commit
-                                withCommit:commit.parents.firstObject
-                               filePattern:nil
-                                   options:(_repository.diffBaseOptions | kGCDiffOption_FindRenames)
-                         maxInterHunkLines:_repository.diffMaxInterHunkLines
-                           maxContextLines:_repository.diffMaxContextLines
-                                     error:NULL];
-    [_diffContentsViewController setDeltas:_currentDiff.deltas usingConflicts:nil];
-  } else {
-    _currentDiff = nil;
-    [_diffContentsViewController setDeltas:nil usingConflicts:nil];
-  }
 }
 
 #pragma mark - GCLiveRepositoryDelegate
@@ -151,11 +112,5 @@
   [self presentError:error];
 }
 
-#pragma mark - GIDiffContentsViewControllerDelegate
-
-- (CGFloat)diffContentsViewController:(GIDiffContentsViewController*)controller headerViewHeightForWidth:(CGFloat)width {
-  NSSize size = [_messageTextField.cell cellSizeForBounds:NSMakeRect(0, 0, width - _messageTextFieldMargins, HUGE_VALF)];
-  return _headerViewMinHeight + size.height;
-}
 
 @end
